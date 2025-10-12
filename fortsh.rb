8e4004c69476145ae25c6ebe879335261aeb0daf
espadonne <espadonne@outlook.com>
2025-10-12 20:32:54 +0000

    Update fortsh to version 3.3.10

--- fortsh.rb
+++ fortsh.rb
@@ -1,8 +1,8 @@
 class Fortsh < Formula
   desc "Modern Unix shell implementation in Fortran with AST-based parsing"
   homepage "https://github.com/FortranGoingOnForty/fortsh"
-  url "https://github.com/FortranGoingOnForty/fortsh/archive/refs/tags/v3.3.9.tar.gz"
-  sha256 "fe19e42adec0cbe600ffff9566a79b0ba79534a43997ea887810b0730259c164"
+  url "https://github.com/FortranGoingOnForty/fortsh/archive/refs/tags/v3.3.10.tar.gz"
+  sha256 "c9546d6ee94ae3bae87231d7a0e9548057790bd33e3cea6c842546d9470ad5c7"
   license "MIT"
   head "https://github.com/FortranGoingOnForty/fortsh.git", branch: "trunk"
 
