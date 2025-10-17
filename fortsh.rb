ce1b05d1edb9274db1ba8abe9e28d431a4f5ce98
espadonne <espadonne@outlook.com>
2025-10-17 22:00:00 +0000

    Update fortsh to 6.0.0 - Full POSIX compliance achieved

--- fortsh.rb
+++ fortsh.rb
@@ -1,8 +1,8 @@
 class Fortsh < Formula
   desc "Modern Unix shell implementation in Fortran with AST-based parsing"
   homepage "https://github.com/FortranGoingOnForty/fortsh"
-  url "https://github.com/FortranGoingOnForty/fortsh/archive/refs/tags/v5.2.10.tar.gz"
-  sha256 "9a87edf0db70e7e458d86a4689cfcc86815df4438f1dfc6eba7d8d3a1b62a815"
+  url "https://github.com/FortranGoingOnForty/fortsh/archive/refs/tags/v6.0.0.tar.gz"
+  sha256 "23054a359bd9d35cf832ebeed1a1e9d051101608d1feeac78b7ddbec20532118"
   license "MIT"
   head "https://github.com/FortranGoingOnForty/fortsh.git", branch: "trunk"
 
